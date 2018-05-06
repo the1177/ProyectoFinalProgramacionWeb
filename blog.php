f02bb986229da74f6c1d02e01895c2e67e2a424b
CesarCruzMunoz <35695493+CesarCruzMunoz@users.noreply.github.com>
2018-05-06 18:41:25 -0500

    Archivos Despues LogIn

---
+++ blog.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/><!-- Para que el texto pueda leer símbolos como la ñ y los acentos-->
+	<meta name="description" content=" Primer Proyecto de Programación Web">
+	<meta name="keywords" content="Universidad">
+	<title>CEJ</title>
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	
+</head>
+
+<body>
+
+	<br></br>
+
+	<header>
+		<div id="menu">
+			<ul> <!-- Agregar etiqueta li -->
+				<a href="blog.php"> Inicio </a>
+				<a href="perfil.php"> Perfil </a>
+				<a href="publi.php"> Publicaciones </a>
+				<a href="logout.php"> Log Out </a>
+			</ul>
+		</div>
+	</header>
+
+	<div id = "centro">
+		<h1> Bienvenido </h1>
+		<br></br>
+		<br></br>
+
+		<img src="img\welcome.jpg", class= 'bienvenido' />  <!-- Ingresar imagen con tamaño y al centro-->
+		
+	</div>
+
+</body>
+
+</html>
---
+++ logout.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/><!-- Para que el texto pueda leer símbolos como la ñ y los acentos-->
+	<meta name="description" content=" Primer Proyecto de Programación Web">
+	<meta name="keywords" content="Universidad">
+	<title>CEJ</title>
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	
+</head>
+
+<body>
+
+	<br></br>
+
+	<header>
+		<div id="menu">
+			<ul> <!-- Agregar etiqueta li -->
+				<a href="index.php"> Index </a>
+				<a href="login.php"> Log In </a>
+				<a href="registro.php"> Register </a>
+			</ul>
+		</div>
+	</header>
+
+	<div id = "centro">
+		<h1> Adios </h1>
+		<br></br>
+		<br></br>
+
+		<!--<img src="img\fuck.jpg", class= 'bienvenido' /> --> <!-- Ingresar imagen con tamaño y al centro-->
+		
+	</div>
+
+</body>
+
+</html>
