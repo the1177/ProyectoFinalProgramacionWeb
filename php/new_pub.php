ff2dbca710569aa5785157a30ffe9968464e73df
CesarCruzMunoz <35695493+CesarCruzMunoz@users.noreply.github.com>
2018-05-07 10:07:16 -0500

    Insertar Publicacion Usuario Sesion

--- php/new_pub.php
+++ php/new_pub.php
@@ -1,12 +1,27 @@
 <?php
-require 'conexion.php';
-//Recibimos datos del html
-$id = "1"; #Modificar con el id del usuario con la sesion iniciada
-$pub = $_POST["pub"];
 
-$insertar = "INSERT INTO publicaciones (id_usuario, publicacion) VALUES ('$id', '$pub')";
-//guardar en BD
-$resultado = mysqli_query($conexion, $insertar);
-mysqli_close($conexion);
-header('location: ../publi.php');
+ 
+    require 'conexion.php';
+        //Recibimos datos del html
+
+    session_start();
+    $sesion_actual= $_SESSION['id'];
+    
+    if(!($sesion_actual)){
+        header('location: index.php');
+        die();	
+    }
+
+    $id = $sesion_actual;
+
+    $pub = $_POST["pub"];
+
+    $insertar = "INSERT INTO publicaciones (id_usuario, publicacion) VALUES ('$id', '$pub')";
+    //guardar en BD
+    $resultado = mysqli_query($conexion, $insertar);
+    mysqli_close($conexion);
+    header('location: ../publi.php');
+
+
+
 ?>
