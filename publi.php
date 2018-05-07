7950c6482aacc2de02ad4699227a95f17b9f659e
CesarCruzMunoz <35695493+CesarCruzMunoz@users.noreply.github.com>
2018-05-07 13:36:09 -0500

    Add files via upload

--- publi.php
+++ publi.php
@@ -1,28 +1,14 @@
-
-
 <?php
-
-	#include("php/new_pub.php");
-
-	session_start();
-	$sesion_actual= $_SESSION['id'];
-
-	if(!($sesion_actual)){
-		header('location: index.php');
-		die();	
-	}
-
+    include("auth.php");
 	require 'php/conexion.php';
 				
 	if(!$conexion ) {
 		die('Could not connect: ' . mysqli_error());
 	}
 
-	#echo 'Connected successfully'."<br>";
-
 	$id = $_SESSION['id']; #Modificar para que sea el id del usuario que tenga la sesion iniciada
 
-	$sql = "SELECT publicacion FROM publicaciones WHERE id_usuario='".$sesion_actual."'";
+	$sql = "SELECT publicacion FROM publicaciones WHERE id_usuario='".$id."'";
 
 	$result = mysqli_query($conexion, $sql);
 ?>
@@ -30,18 +16,27 @@
 
 <!DOCTYPE html>
 <html>
+
+
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/><!-- Para que el texto pueda leer símbolos como la ñ y los acentos-->
-	<meta name="description" content=" Primer Proyecto de Programación Web">
-	<meta name="keywords" content="Universidad">
-	<title>CEJ</title>
-	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<!-- Para que el texto pueda leer símbolos como la ñ y los acentos-->
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 	
+	<meta name="description" content="Primer Proyecto de Programación Web">
+	<meta name="keywords" content="Universidad">
+			
+	<title>Publicaciones</title>
+			
+	<!-- Icono de Favicon -->
+	<link rel="shortcut icon" type="image/ico" href="/img/favicon.ico"/>
+			
+	<!-- Hoja de estilos -->
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
 </head>
 
 <body>
 
-	<br></br>
+	<br/>
 
 	<header>
 		<div id="menu">
@@ -55,23 +50,20 @@
 	</header>
 
 	<div id = "centro">
-
-		<form method="post" action="php/new_pub.php" class="form-register"> 
+		<form action="php/new_pub.php" method="post" class="form-register"> 
 			<h2 class="form_titulo">
 				Nueva Publicacion
 			</h2>
 
 			<div class="contenedor-inputs">
 				<input type="text" id="pub" name="pub" placeholder="Publicacion" class="input-100">
-				<input type="submit" value="Publish" class="btn-enviar" >
+				<input type="submit" value="Publish" class="btn-enviar" onclick= "validar();">
 			</div>
-
 		</form>
 
 		<h1> Publicaciones </h1>
-		<br></br>
+		<br/>
 
-	<!--	!-->
 	<table width="70%" border="1px" align="center">
 
 		<?php 
